134dbcafe591d319f0278127ff5cf6faa4168f01
github username <github email address>
2020-03-02 08:16:00 -0600

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
   end
 
   def add
+    #this cart is from the helper method in the application controller with the session[:product]
     cart << params[:product]
     render 'index'
   end
